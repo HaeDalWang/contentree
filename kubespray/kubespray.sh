c45e0b538abb8748357ee8e308072b371a2f636c
root <root@ip-10-150-44-53.ap-northeast-2.compute.internal>
2023-03-28 04:50:58 +0000

    CIS 플레이북 추가

--- kubespray/kubespray.sh
+++ kubespray/kubespray.sh
@@ -3,7 +3,9 @@ docker run \
 --network=host \
 --detach \
 --restart=always \
---mount type=bind,source="$(pwd)",dst=/kubespray/inventory \
+--mount type=bind,source="$(pwd)/inventory",dst=/kubespray/inventory \
+--mount type=bind,source="$(pwd)/extra_playbooks",dst=/kubespray/extra_playbooks \
+--mount type=bind,source="$(pwd)/artifacts",dst=/kubespray/artifacts \
 --mount type=bind,source=/root/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
 --env ANSIBLE_HOST_KEY_CHECKING=False \
 quay.io/kubespray/kubespray:v2.21.0 sleep infinity
